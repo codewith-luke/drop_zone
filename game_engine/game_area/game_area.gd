e5e5c765fd01ee5b901518a1cfbca40a9faf48b1
Luke Babich <luke@coldbrewstudios.co.uk>
2024-06-05 20:32:37 +0100

    feat: adjusted game area

--- game_engine/game_area/game_area.gd
+++ game_engine/game_area/game_area.gd
@@ -8,27 +8,25 @@ extends Node
 var players = 0
 var player_lobby = {} 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	HEATMAP_NODE.on_player_heatmap_click.connect(_on_player_add)
 	print("loaded game area")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
 
 func _on_add_player_pressed():	
+	print("clicked")
 	var mouse_position = get_viewport().get_mouse_position()
 	_on_player_add(mouse_position)
 	
 func _on_player_add(position):
 	var player_ball = player_ball.instantiate()
-	var collision_entity = player_ball.get_node("RigidBody2D/CollisionShape2D")
+	var collision_entity = player_ball.get_node("CollisionShape2D")
 	var ball_size = collision_entity.get_shape().get_rect().size
 	position.y = position.y - ball_size.x;
 	position.x = position.x - (ball_size.y / 2);
 	player_ball.position = position
-	print("size: ", player_ball.position)
 	players += 1
 	add_child(player_ball)
 	HUD_NODE.update_player_count(players);
--- game_engine/player_ball/player_ball.gd
+++ game_engine/player_ball/player_ball.gd
@@ -1,11 +1,11 @@
-extends Node
+extends RigidBody2D
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$VisibleOnScreenNotifier2D.screen_exited.connect(_on_visible_on_screen_notifier_2d_screen_exited)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	print("deleted node")
+	queue_free()
